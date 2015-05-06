b8893518524035fa49f1679f30a3ec62b328a1e0
Valerio Cietto <valerio_ciet@hotmail.it>
2015-05-06 16:36:23 +0200

    Update TwoServoRobot.ino

--- TwoServoRobot.ino
+++ TwoServoRobot.ino
@@ -1,4 +1,4 @@
-/* 
+* 
  Two legged robot
  @Author Valerio Cietto
 */
@@ -8,21 +8,25 @@
 Servo front;  // create servo object to control a servo
 Servo rear;
 
-
+int del=50;
 void setup()
 {
   front.attach(3);  // attaches the servo on pin 3 to the servo object
   rear.attach(6);  // attaches the servo on pin 6 to the servo object
+  front.write(90);
+  rear.write(90);
+  
+  
 }
 
 void loop() 
 { 
-  front.write(180);
-  delay(1000);
-  rear.write(180);
-  delay(1000);
-  front.write(0);
-  delay(1000);
-  rear.write(0);
-  delay(1000);
+  front.write(60);
+  delay(del);
+  rear.write(60);
+  delay(del);
+  front.write(120);
+  delay(del);
+  rear.write(120);
+  delay(del);
 } 
